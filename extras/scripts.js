db1ed7b7a54604828f5d8b3e6ceb28081c7b2121
Tobias-André <tobias.hole@gmail.com>
2024-01-11 13:09:30 +0100

    Mjau

---
+++ extras/scripts.js
@@ -0,0 +1,10 @@
+function easter1() {
+    location.assign('https://www.youtube.com/watch?v=dQw4w9WgXcQ')
+}
+
+function easter2() {
+    location.assign('https://www.youtube.com/watch?v=79-AwFZCKpA')
+}
+function angy() {
+    
+}
