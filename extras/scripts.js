62e7cb4ff02ad1c983cf07e98f7d565e51b0d1be
Tobias-André <tobias.hole@gmail.com>
2024-01-12 09:05:18 +0100

    Added comments, and a tiny bit of function.

--- extras/scripts.js
+++ extras/scripts.js
@@ -1,15 +1,19 @@
+//Redirects page to rickroll
 function easter1() {
     location.assign('https://www.youtube.com/watch?v=dQw4w9WgXcQ');
 }
 
+//Redirects page to Josh Hutcherson
 function easter2() {
     location.assign('https://www.youtube.com/watch?v=79-AwFZCKpA');
 }
 
+//Redirects page to angry page
 function angy() {
     location.assign('angy.html');
 }
 
+//Redirects page to main page
 function goToMain() {
     location.assign('main.html');
 }
