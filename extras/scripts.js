5cd797d45ebd68319006297ba9a759dd30d12d77
Tobias André <tobias.hole@gmail.com>
2024-01-12 08:33:37 +0100

    Yuhh

--- extras/scripts.js
+++ extras/scripts.js
@@ -1,10 +1,15 @@
 function easter1() {
-    location.assign('https://www.youtube.com/watch?v=dQw4w9WgXcQ')
+    location.assign('https://www.youtube.com/watch?v=dQw4w9WgXcQ');
 }
 
 function easter2() {
-    location.assign('https://www.youtube.com/watch?v=79-AwFZCKpA')
+    location.assign('https://www.youtube.com/watch?v=79-AwFZCKpA');
 }
+
 function angy() {
-    
+    location.assign('angy.html');
+}
+
+function back() {
+    location.assign('index.html');
 }
